3532b765f303f82f71a82a8166399b9f82db2b68
Darren Churchill <darrenmchurchill@gmail.com>
2024-04-20 14:30:44 -0700

    Refactor using Contact class

---
+++ contact.js
@@ -0,0 +1,15 @@
+/**
+ * JS175 Project
+ * Contacts
+ * contact.js
+ */
+
+class Contact {
+  constructor(firstName, lastName, phoneNumber) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.phoneNumber = phoneNumber;
+  }
+}
+
+module.exports = Contact;
--- contacts.js
+++ contacts.js
@@ -6,30 +6,19 @@
 const express = require("express");
 const morgan = require("morgan");
 
+const Contact = require("./contact");
+
 const PORT = 3000;
 const app = express();
 
+// NOTE: You could also use app.locals to store this contact data and express
+// will automatically make the variable available to the template engine.
+// https://expressjs.com/en/4x/api.html#app.locals
 let contactData = [
-  {
-    firstName: "Mike",
-    lastName: "Jones",
-    phoneNumber: "281-330-8004",
-  },
-  {
-    firstName: "Jenny",
-    lastName: "Keys",
-    phoneNumber: "768-867-5309",
-  },
-  {
-    firstName: "Max",
-    lastName: "Entiger",
-    phoneNumber: "214-748-3647",
-  },
-  {
-    firstName: "Alicia",
-    lastName: "Keys",
-    phoneNumber: "515-489-4608",
-  },
+  new Contact("Mike", "Jones", "281-330-8004",),
+  new Contact("Jenny", "Keys", "768-867-5309",),
+  new Contact("Max", "Entiger", "214-748-3647",),
+  new Contact("Alicia", "Keys", "515-489-4608",),
 ];
 
 const sortContacts = (contacts) => {
