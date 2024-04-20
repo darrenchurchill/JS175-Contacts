3fbbfb139ad43e2dc9176c3bf53d32b86083606b
Darren Churchill <darrenmchurchill@gmail.com>
2024-04-20 13:22:03 -0700

    5.12: Add initial files

--- contacts.js
+++ contacts.js
@@ -3,3 +3,60 @@
  * Contacts
  * contacts.js
  */
+const express = require("express");
+const morgan = require("morgan");
+
+const PORT = 3000;
+const app = express();
+
+let contactData = [
+  {
+    firstName: "Mike",
+    lastName: "Jones",
+    phoneNumber: "281-330-8004",
+  },
+  {
+    firstName: "Jenny",
+    lastName: "Keys",
+    phoneNumber: "768-867-5309",
+  },
+  {
+    firstName: "Max",
+    lastName: "Entiger",
+    phoneNumber: "214-748-3647",
+  },
+  {
+    firstName: "Alicia",
+    lastName: "Keys",
+    phoneNumber: "515-489-4608",
+  },
+];
+
+const sortContacts = (contacts) => {
+  return contacts.slice().sort((contactA, contactB) => {
+    if (contactA.lastName === contactB.lastName) {
+      return contactA.firstName.localeCompare(contactB.firstName);
+    }
+    return contactA.lastName.localeCompare(contactB.lastName);
+  });
+};
+
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+app.use(express.static("public"));
+app.use(morgan("common"));
+
+app.get("/", (_req, res) => {
+  res.redirect("/contacts");
+});
+
+app.get("/contacts", (_req, res) => {
+  res.render("contacts", {
+    contacts: sortContacts(contactData),
+  });
+});
+
+app.listen(PORT, "localhost", () => {
+  console.log(`Server listening on port ${PORT}...`);
+});
